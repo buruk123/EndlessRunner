082c474ce7005d04ef0fe9d907f930624879b5fa
Rafał Wawiórka <rafal.wawiorka@gmail.com>
2020-02-18 09:57:15 +0100

    Added back to menu button on highscores scene

---
+++ Assets/Scripts/HighscoresScripts/BackToMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackToMenu : MonoBehaviour
+{
+    [SerializeField] private Button backButton;
+
+    private void Start()
+    {
+        backButton.onClick.AddListener(BackToMainMenu);
+    }
+
+    private void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
